e26feb99da93d3433c0932f361d93ed88075f8ac
Tonnie <vontonnie@gmail.com>
2022-07-10 21:34:19 +0300

    Add WeatherData model class

---
+++ app/src/main/java/com/plcoding/weatherapp/domain/model/WeatherData.kt
@@ -0,0 +1,13 @@
+package com.plcoding.weatherapp.domain.model
+
+import com.plcoding.weatherapp.domain.weather.WeatherType
+import java.time.LocalDateTime
+
+data class WeatherData(
+    val time: LocalDateTime,
+    val temperatureCelsius: Double,
+    val pressure: Double,
+    val windSpeed: Double,
+    val humidity: Double,
+    val weatherType: WeatherType
+)
