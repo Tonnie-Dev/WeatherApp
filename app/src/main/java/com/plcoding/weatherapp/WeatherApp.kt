379e4bea787297957636e403098b90a37d5fe991
Tonnie <vontonnie@gmail.com>
2022-07-11 20:04:44 +0300

    Add WeatherState data class in the overview_screen package

--- app/src/main/java/com/plcoding/weatherapp/WeatherApp.kt
+++ app/src/main/java/com/plcoding/weatherapp/WeatherApp.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
-@HiltAndroidApp
+@HiltAndroidApp //this tells hilt where to get the application context from
 class WeatherApp:Application() {
 
     override fun onCreate() {
---
+++ app/src/main/java/com/plcoding/weatherapp/presentation/overview_screen/WeatherState.kt
@@ -0,0 +1,9 @@
+package com.plcoding.weatherapp.presentation.overview_screen
+
+import com.plcoding.weatherapp.domain.weather.WeatherInfo
+
+data class WeatherState(
+    val weatherInfo: WeatherInfo? = null,
+    val isLoading: Boolean = false,
+    val error: String? = null
+)
