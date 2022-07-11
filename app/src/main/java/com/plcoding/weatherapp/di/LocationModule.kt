0dfa44b0c8ca31f0540079687c5f6435b62c1ada
Tonnie <vontonnie@gmail.com>
2022-07-11 19:45:00 +0300

    Add Location module abstract class in the di package

---
+++ app/src/main/java/com/plcoding/weatherapp/di/LocationModule.kt
@@ -0,0 +1,17 @@
+package com.plcoding.weatherapp.di
+
+import android.app.Application
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class LocationModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindLocationTracker()
+}
