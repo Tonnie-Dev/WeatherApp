ea9a062f90cac78e867ffdea3ef68824a8f348c8
Tonnie <vontonnie@gmail.com>
2022-07-11 19:55:38 +0300

    Initialize Timber in WeatherApp's oncreate()

--- app/src/main/java/com/plcoding/weatherapp/WeatherApp.kt
+++ app/src/main/java/com/plcoding/weatherapp/WeatherApp.kt
@@ -2,11 +2,14 @@ package com.plcoding.weatherapp
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class WeatherApp:Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
     }
 }
