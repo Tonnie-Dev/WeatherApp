3cc24f16a12b4cc2668476dc012c1a784c25f670
Tonnie <vontonnie@gmail.com>
2022-07-12 19:15:00 +0300

    Add Card interface composable

--- app/src/main/java/com/plcoding/weatherapp/presentation/overview_screen/WeatherCard.kt
+++ app/src/main/java/com/plcoding/weatherapp/presentation/overview_screen/WeatherCard.kt
@@ -1,8 +1,38 @@
 package com.plcoding.weatherapp.presentation.overview_screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun WeatherCard() {
-    
+fun WeatherCard(
+    state: WeatherState,
+    background: Color,
+    modifier: Modifier = Modifier
+) {
+
+    //show the card only when current weather is not null
+
+    state.weatherInfo?.currentWeatherData?.let {
+        data ->
+
+        Card(
+                backgroundColor = background,
+                shape = RoundedCornerShape(10.dp),
+                modifier = modifier.padding(16.dp)
+        ) {
+
+            Column(modifier = Modifier.fillMaxWidth().padding(16.dp)) {
+
+            }
+        }
+    }
+
+
 }
