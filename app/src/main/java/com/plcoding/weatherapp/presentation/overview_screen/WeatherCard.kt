c6e50e02d3f7ff79b9fc98ed147cdd81c46476b2
Tonnie <vontonnie@gmail.com>
2022-07-12 19:23:10 +0300

    Add Text for the hour

--- app/src/main/java/com/plcoding/weatherapp/presentation/overview_screen/WeatherCard.kt
+++ app/src/main/java/com/plcoding/weatherapp/presentation/overview_screen/WeatherCard.kt
@@ -5,10 +5,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import java.time.format.DateTimeFormatter
 
 @Composable
 fun WeatherCard(
@@ -19,8 +21,7 @@ fun WeatherCard(
 
     //show the card only when current weather is not null
 
-    state.weatherInfo?.currentWeatherData?.let {
-        data ->
+    state.weatherInfo?.currentWeatherData?.let { data ->
 
         Card(
                 backgroundColor = background,
@@ -28,8 +29,18 @@ fun WeatherCard(
                 modifier = modifier.padding(16.dp)
         ) {
 
-            Column(modifier = Modifier.fillMaxWidth().padding(16.dp)) {
+            Column(
+                    modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+            ) {
 
+
+                Text(
+                        text = "Today ${data.time.format(DateTimeFormatter.ISO_DATE_TIME)}",
+                        color = Color.White,
+                        modifier = Modifier.align()
+                )
             }
         }
     }
