5160624832a65e068deffb39f64b34c5a7b41067
Tonnie <vontonnie@gmail.com>
2022-07-13 07:26:12 +0300

    Include contents for the WeatherDataDisplay composable

--- app/src/main/java/com/plcoding/weatherapp/presentation/overview_screen/WeatherDataDisplay.kt
+++ app/src/main/java/com/plcoding/weatherapp/presentation/overview_screen/WeatherDataDisplay.kt
@@ -1,8 +1,41 @@
 package com.plcoding.weatherapp.presentation.overview_screen
 
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun WeatherDataDisplay() {
-    
+fun WeatherDataDisplay(
+    value: Int,
+    unit: String,
+    icon: ImageVector,
+    modifier: Modifier = Modifier,
+    textStyle: TextStyle = TextStyle(),
+    iconTint: Color = Color.White
+) {
+    Row(
+            modifier = modifier,
+            verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        Icon(
+                imageVector = icon,
+                contentDescription = null,
+                modifier = Modifier.size(25.dp),
+                tint = iconTint
+        )
+        Spacer(modifier = Modifier.width(4.dp))
+        Text(text = "$value$unit", style = textStyle)
+    }
 }
